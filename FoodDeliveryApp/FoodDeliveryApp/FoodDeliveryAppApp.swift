b56bcf1cdccbeeaf3020c1401a97583eac212600
Vlad Ch <vividimagelogin@gmail.com>
2023-03-02 13:34:10 +0200

    Project created

---
+++ FoodDeliveryApp/FoodDeliveryApp/FoodDeliveryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryAppApp.swift
+//  FoodDeliveryApp
+//
+//  Created by admin on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
