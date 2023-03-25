6dab41ff9a99f94964211b18cf435cb6ef9b67ea
Vlad Ch <vividimagelogin@gmail.com>
2023-03-25 19:35:30 +0200

    adding annotation

--- FoodDeliveryApp/Source/Cart/Delivery/Views/DeliveryMapViewRepresentable.swift
+++ FoodDeliveryApp/Source/Cart/Delivery/Views/DeliveryMapViewRepresentable.swift
@@ -17,7 +17,6 @@ struct DeliveryMapViewRepresentable: UIViewRepresentable {
     func makeUIView(context: Context) -> some UIView {
         mapView.delegate = context.coordinator
         mapView.isRotateEnabled = false
-        mapView.showsUserLocation = true
         mapView.userTrackingMode = .follow
         
         return mapView
@@ -25,9 +24,12 @@ struct DeliveryMapViewRepresentable: UIViewRepresentable {
     
     func updateUIView(_ uiView: UIViewType, context: Context) {
         if let coordinate = locationSearchViewModel.selectedLocationCoordinate {
-            print("DEBUG: selectedLocation - \(coordinate)")
-            let newCoordinateRegion = MKCoordinateRegion(center: coordinate, span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02))
+            let newCoordinateRegion = MKCoordinateRegion(
+                center: coordinate,
+                span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+            )
             mapView.setRegion(newCoordinateRegion, animated: true)
+            context.coordinator.addAndSelectAnnotation(withCoordinate: coordinate)
         }
     }
     
@@ -52,14 +54,24 @@ extension DeliveryMapViewRepresentable {
         // MARK: - MKMapViewDelegate
         
         func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
-            let region = MKCoordinateRegion(
-                center: CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude),
-                span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
-            )
+            let coordinate = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+            let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+            let region = MKCoordinateRegion(center: coordinate, span: span)
             
             parent.mapView.setRegion(region, animated: true)
+            addAndSelectAnnotation(withCoordinate: coordinate)
         }
         
         // MARK: - Helpers
+        
+        func addAndSelectAnnotation(withCoordinate coordinate: CLLocationCoordinate2D) {
+            parent.mapView.removeAnnotations(parent.mapView.annotations)
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            parent.mapView.addAnnotation(annotation)
+            parent.mapView.selectAnnotation(annotation, animated: true)
+        }
+        
     }
 }
